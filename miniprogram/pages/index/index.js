4d6a6c08c265cd8c4e4e9fa916b08543dc4ab6e0
shichenhang <181250124@smail.nju.edu.cn>
2020-05-24 15:44:53 +0800

    数据库储存文章信息（具体文章是一个page）

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -5,22 +5,25 @@ Page({
    * 页面的初始数据
    */
   data: {
-    articleList:[{route:"../articles/article1/article1",title:"张文宏建议不要喝粥，是怎么回事",brief:"粥的营养价值有限，除了碳水化合物，我们还需要蛋白质、脂肪、维生素、电解质等营养物质，而单纯喝粥是远远不够的。"},
+    /*
+    articles:[
+      {route:"../articles/article1/article1",title:"张文宏建议不要喝粥，是怎么回事",brief:"粥的营养价值有限，除了碳水化合物，我们还需要蛋白质、脂肪、维生素、电解质等营养物质，而单纯喝粥是远远不够的。"},
   ]
+  */
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getArticleList()
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
@@ -48,7 +51,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+    this.getArticleList()
   },
 
   /**
@@ -63,6 +66,19 @@ Page({
    */
   onShareAppMessage: function () {
 
+  },
+
+  getArticleList:function(){
+    wx.cloud.callFunction({
+      name:'getArticleList',
+    }).then(res=>{
+      console.log(res.result.data)
+      this.setData({articles:res.result.data})
+  })
+ /*
+ const db=wx.cloud.database()
+ db.collection('articles').get().then(res=>{console.log(res.data)})
+ */
   },
 
   goToArticle: function(event){
