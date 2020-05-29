e0c77b9c7b79e3b7285334ef298d36a5818afa9a
Chao-s <603352407@qq.com>
2020-05-30 04:46:35 +0800

    my-version1
    身高体重数据和饮食方案列表的大体框架，具备计算bmi和删除方案功能

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -7,25 +7,36 @@ Page({
   data: {
     height:null,
     weight:null,
-    bmi:0,
-    dietList:[{title:"饮食方案名称",context:"内容",note:"备注"}]
+    bmi:"现在的数据计算不出yo",
+    dietList:[
+    {title:"饮食方案名称1",brief:"简介1",context:"内容1",note:"备注1"},
+    {title:"饮食方案名称2",brief:"简介2",context:"内容2",note:"备注2"},
+    {title:"饮食方案名称3",brief:"简介3",context:"内容3",note:"备注3"},
+    {title:"饮食方案名称4",brief:"简介4",context:"内容4",note:"备注4"},
+    {title:"饮食方案名称5",brief:"简介5",context:"内容5",note:"备注5"}
+    ]
   },
 
   calBMI: function(){
     console.log("calBMI");
     var height = this.data.height;
     var weight = this.data.weight;
-    if(height&&weight){
-      console.log("height,weight");
+    if(height&&weight&&height>0&&weight>0){
+      console.log(height,weight);
       this.setData({
-        bmi:height==0?0:weight/(height*height)
+        bmi:weight/(height*height)
+      });
+    }else{
+      console.log("calBMI failed");
+      this.setData({
+        bmi:"现在的数据计算不出yo"
       });
     }
   },
 
   inputData:function(event){
     console.log(event);
-    let name = event.currentTarget.dataset.name;
+    var name = event.currentTarget.dataset.name;
     this.setData({
       [name]:event.detail.value
     });
@@ -38,6 +49,16 @@ Page({
     wx.navigateTo({
       url: `../detail/detail?title=${dataset.title}&context=${dataset.context}&note=${dataset.note}`
     })
+  },
+
+  deleteDiet:function(event){
+    console.log(event);
+    var list = this.data.dietList;
+    var index = event.currentTarget.dataset.index;
+    list.splice(index,1);
+    this.setData({
+      dietList:list
+    });
   },
 
   /**
