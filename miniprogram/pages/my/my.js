f18c9b9d8ad53a0e94ba50572194ef7ea2077514
Chao-s <603352407@qq.com>
2020-05-30 05:53:03 +0800

    my-version2
    删除了bmi计算按钮，改为即时计算；在my.js提供了添加饮食方案的函数和接口说明供cal使用；照搬article糊弄了下detail页面的格式

--- miniprogram/pages/my/my.js
+++ miniprogram/pages/my/my.js
@@ -8,6 +8,10 @@ Page({
     height:null,
     weight:null,
     bmi:"现在的数据计算不出yo",
+    // 需要pages/cal中存在添加dietList的功能，cal的逻辑设计取决于下方接口描述
+    // 传递的数据包括下方dietList实例中的四个属性，其中title为主键，cal在插入数据时要检查title是否重复
+    // 已有addDiet函数且功能包括title检查，但不确定是否正确以及可用
+    // detail详情页中context的显示方式为<text decode="{{true}}" space="nbsp">{{diet.context}}</text>
     dietList:[
     {title:"饮食方案名称1",brief:"简介1",context:"内容1",note:"备注1"},
     {title:"饮食方案名称2",brief:"简介2",context:"内容2",note:"备注2"},
@@ -17,6 +21,7 @@ Page({
     ]
   },
 
+  //计算bmi
   calBMI: function(){
     console.log("calBMI");
     var height = this.data.height;
@@ -34,14 +39,18 @@ Page({
     }
   },
 
+  //如果my.wxml中改为直接用双向绑定输入数据，则注释“失去焦点时才调用”的
+  //这个inputData函数，并恢复my.wxml中bmi的计算按钮
   inputData:function(event){
     console.log(event);
     var name = event.currentTarget.dataset.name;
     this.setData({
       [name]:event.detail.value
     });
+    this.calBMI();
   },
 
+  // 跳转向detail页面的同时传去“标题、内容和备注”三项数据
   goToDetail: function(event){
     console.log(event)
     var dataset = event.currentTarget.dataset;
@@ -51,11 +60,28 @@ Page({
     })
   },
 
+  //按index删除饮食方案列表中的一项
   deleteDiet:function(event){
     console.log(event);
     var list = this.data.dietList;
     var index = event.currentTarget.dataset.index;
     list.splice(index,1);
+    this.setData({
+      dietList:list
+    });
+  },
+
+  //不确定是否正确的addDiet函数
+  addDiet:function(newDiet){
+    console.log("addDiet");
+    var list = this.data.dietList;
+    for(diet in list){
+      if(diet.title==newDiet.title){
+        console.log("insert failed");
+        return;
+      }
+    }
+    list.push(newDiet);
     this.setData({
       dietList:list
     });
