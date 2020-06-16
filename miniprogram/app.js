9170cc71659e69a5be9d6f518f4ae13a30af8dd6
wjw <181250135@smail.nju.edu.cn>
2020-06-16 09:28:32 +0800

    更改第二页面样式

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -19,57 +19,6 @@ App({
       traceUser: true,
     })
 
-    this.globalData = {
-      //dietList中是供测试用的diet项,投入使用前需将dietList表清空
-      dietList:[
-        {title:"饮食方案名称1",calorie:"calorie1",context:"内容1",note:""},
-        {title:"饮食方案名称2",calorie:"calorie2",context:"内容2",note:"0"},
-        {title:"饮食方案名称3",calorie:"calorie3",context:"内容3",note:undefined},
-        {title:"饮食方案名称4",calorie:"calorie4",context:"内容4",note:"null"},
-        {title:"饮食方案名称5",calorie:"calorie5",context:"内容5",note:"备注5"}
-        ]
-    }
-  },
-
-  //利用setter/getter访问器绑定my中page的引用到app中updateDiet属性上，
-  //使得updateDiet的访问器被调用时my立刻发现并做出即时行为(如刷新my页面中dietList),实现watch监听函数效果
-  watchDietList:function(method){
-    console.log("watch added to dietList");
-    var obj = this.globalData;
-    Object.defineProperty(obj,"updateDiet", {
-      configurable: true,
-      enumerable: true,
-      set: function (value) {
-        console.log("利用updateDiet的setter更新my页面(",value,"未使用)");
-        method();
-      },
-      get:function(){
-        console.log("利用updateDiet的getter更新my页面(返回值为null)");
-        method();
-        return null;
-      }
-    })
-  },
-
-  add_diet:function(newDiet){
-    console.log("添加饮食方案:",newDiet);
-    if(!this.uniq_diet(newDiet.title)){
-      console.log("insert failed");
-      return false;
-    }
-    var list = this.globalData.dietList;
-    list.push(newDiet);
-    this.globalData.updateDiet;
-    return true;
-  },
-
-  uniq_diet:function(title){
-    var list = this.globalData.dietList;
-    for(let i=0;i<list.length;i++){
-      if(list[i].title==title){
-        return false;
-      }
-    }
-    return true;
+    this.globalData = {}
   }
 })
--- miniprogram/pages/cal/cal.js
+++ miniprogram/pages/cal/cal.js
@@ -1,181 +1,666 @@
-// miniprogram/pages/cal/cal.js
+
+var shopList = require('./shopList.js');
+
+var app = getApp();
+
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-    carts: [],
-    list: [],
 
-    list01: [
-      { item_id: 1 }, { item_id: 11 }, { item_id: 11 },
-    ],
-    list02: [
+    showCartDetail: false,
 
-    ],
-    list03: [
-      { item_id: 11 }, { item_id: 11 }
-    ],
-    list04: [
-      { item_id: 11 }, { item_id: 11 }, { item_id: 11 }
-    ],
+    list: [],//商品列表
 
-  // 展开折叠
-    selectedFlag: [false, false, false, false],
+    classifyViewed: '',//默认顶部
+
+    buyNum: {},
+
+    cart: [],
+
+    surplusNum: {},
+
+    sumMoney: 0.00, //购买总价
+
+    buySum: 0//购买总数
 
   },
-    // 展开折叠选择  
-  changeToggle:function(e){
-    var index = e.currentTarget.dataset.index;
-    if (this.data.selectedFlag[index]){
-      this.data.selectedFlag[index] = false;
-    }else{
-      this.data.selectedFlag[index] = true;
+
+
+
+  /**
+
+   * 初始加载获取商品数据
+
+   */
+
+  onLoad: function () {
+
+    //请求后台得来的商品list，在这里就不写怎么获取的
+
+    //在同级包下写了一个数据的js先引用讲解
+
+    var list = shopList.List;
+
+    // 购买商品的数量
+
+    var data = { buyNum: {} };
+
+    // 判断商品剩余使用，商品id为键 商品剩余为值
+
+    var surplusNum = {};
+
+    for (let i in list) {
+
+      var id = list[i].id;
+
+      // 更换商品种类（id不能以数字开头）
+
+      list[i].id = 'a' + id;
+
+      var goods = list[i].goods;
+
+      for (let j in goods) {
+
+        //拼接商品的图片,这里写死了开发过程中要写成配置文件
+
+        goods[j].image = 'http://127.0.0.1/object/' + goods[j].image;
+
+
+
+        data.buyNum[goods[j].id] = 0;
+
+        surplusNum[goods[j].id] = goods[j].surplusNum;//判断商品剩余
+
+      }
+
     }
 
+    //原始list存入data中方便页面遍历
+
+    data.list = list;
+
+    this.setData(data);
+
     this.setData({
-      selectedFlag: this.data.selectedFlag
-    })
-  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+      surplusNum: surplusNum,
 
-  },
+      //作为一个比较的商品剩余
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
+      surplusnumInfo: surplusNum,
 
-  },
+      //默认一个商品种类选中
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+      classifySeleted: list[0].id
 
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
-   a: function () {
-
-  },
-  input(e) {
-    this.search(e.detail.value)
-  },
-  search(key) {
-    var that = this;
-    //从本地缓存中异步获取指定 key 的内容
-    var list = wx.getStorage({
-      key: 'list',
-      //从Storage中取出存储的数据
-      success: function (res) {
-        // console.log(res)
-        if (key == '') { //用户没有输入时全部显示
-          that.setData({
-            list: res.data
-          })
-          return;
-        }
-        var arr = []; //临时数组，用于存放匹配到的数组
-        for (let i in res.data) {
-          if (res.data[i].name.indexOf(key) >= 0) {
-            arr.push(res.data[i])
-          }
-        }
-        if (arr.length == 0) {
-          that.setData({
-            list: [{ name: '没有相关数据！' }]
-          })
-        } else {
-          that.setData({
-            list: arr
-          })
-        }
-      },
-    })
-  },
-  /**
-  * 生命周期函数--监听页面加载
-  */
-  onLoad: function (options) {
-    var list = [
-      { name: "大米" },
-      { name: "小米" },
-      { name: "面条" },
-      { name: "猪肉" }
-    ]
-    wx.setStorage({
-      key: 'list',
-      data: list
-    })
-    this.setData({
-      list: list
-    })
-  },
-  
-  //以下为向my的dietList添加diet的函数接口，修改后即可使用
-  //以及为啥cal页面写了两个onLoad
-  bindCheckout:function(){
-    //数据格式如下diet所示，均为字符串类型，其中title为主键，title重复时会插入失败
-    //detail详情页中context的显示方式为<text decode="{{true}}" space="nbsp">{{diet.context}}</text>
-    let newDiet = {
-      title:"饮食方案名称_add1",//主键
-      calorie:"calorie_add1",//需要自带单位
-      context:"内容_add1",//由选定食物组成，仅在detail页面显示
-      note:""//为食谱的备注，该功能不实现时note需要取""(此时detail页面备注为"无")，仅在detail页面显示
-    };
-
-    //添加diet功能实现如下所示,res返回添加是否成功的布尔值
-    let app = getApp(); 
-    const res = app.add_diet(newDiet);
-    console.log("更新是否成功:",res);
-
-    //添加失败的处理如下(如提醒添加失败)
-    if(!res){
-      //仅示例，可替换为更好的处理
-      wx.showToast({
-        title: "标题已存在哦",//最多七个汉字长度
-        icon: "none",
-        duration: 2000
     });
+
+  },
+
+
+
+  //加
+
+  add: function (e) {
+
+    var detail = e.currentTarget.dataset.detail;//是否来自商品列表还是购物车
+
+    var productId, classGoodsIndex, goodsIndex, image, rebatePrice, price, productName;
+
+    if (detail == 'cart') {//购物车的加号
+
+      productId = e.currentTarget.dataset.id;
+
+      productName = e.currentTarget.dataset.name;
+
+      price = e.currentTarget.dataset.price;
+
+    } else {
+
+      productId = e.target.dataset.id;
+
+      classGoodsIndex = e.target.dataset.classgoodsindex;//商品种类的下标
+
+      goodsIndex = e.target.dataset.goodsindex;//商品的下标
+
+      image = this.data.list[classGoodsIndex].goods[goodsIndex].image;//商品图片
+
+      rebatePrice = this.data.list[classGoodsIndex].goods[goodsIndex].rebatePrice;//优惠价格
+
+      price = this.data.list[classGoodsIndex].goods[goodsIndex].standardPrice;//售价
+
+      productName = this.data.list[classGoodsIndex].goods[goodsIndex].name;//商品名字
+
     }
+
+    //判断优惠价格,没有util包先简单判断一下
+
+    if (rebatePrice != null && rebatePrice != '') {
+
+      price = rebatePrice;
+
+    }
+
+    var arrId = [];//商品id集合
+
+    var arr = this.data.cart || [];
+
+    //判断商品剩余(第一次加入购物车肯定没有所以加一)
+
+    if (this.data.buyNum[productId] + 1 > this.data.surplusnumInfo[productId]) {
+
+      wx.showToast({
+
+        icon: 'none',
+
+        title: '商品剩余数量不足',
+
+      })
+
+      return false;
+
+    }
+
+    var buyCount = 1;
+
+    //第一次加入商品
+
+    if (arr.length == 0) {
+
+      arr.push({
+
+        id: productId,
+
+        name: productName,
+
+        price: price,
+
+        buyCount: buyCount,
+
+        buyMoney: this.buyOneMoney(price, buyCount),
+
+        standardSellingPrice: this.data.list[classGoodsIndex].goods[goodsIndex].standardPrice,
+
+        image: image
+
+      });
+
+    } else if (arr.length >= 1) {
+
+      //商品id集合
+
+      for (let g in arr) {
+
+        arrId.push(arr[g].id);
+
+      }
+
+      //商品已存在的判断
+
+      if (arrId.indexOf(productId) != -1) {
+
+        for (let g in arr) {
+
+          if (arr[g].id == productId) {
+
+            arr[g].buyCount += 1;
+
+            buyCount = arr[g].buyCount;
+
+            arr[g].buyMoney = this.buyOneMoney(arr[g].price, arr[g].buyCount)
+
+            break;
+
+          }
+
+        }
+
+      } else {
+
+        arr.push({
+
+          id: productId,
+
+          name: productName,
+
+          price: price,
+
+          buyCount: buyCount,
+
+          buyMoney: this.buyOneMoney(price, buyCount),
+
+          image: image
+
+        });
+
+      }
+
+    }
+
+    var data = { buyNum: this.data.buyNum };
+
+    data.buyNum[productId] = buyCount;
+
+    //用来显示加减购物的数量显示
+
+    this.data.buyNum[productId] = buyCount;
+
+    this.setData(data);
+
+
+
+    //用于底部显示
+
+    this.setData({
+
+      sumMoney: this.buySumMoney(arr),
+
+      cart: arr,
+
+      buySum: this.buySum(arr),
+
+    });
+
+    //清空商品id集合
+
+    arrId = [];
+
+    //判断商品剩余与购买的数量
+
+    var surplusnum = this.data.surplusNum;
+
+    for (var i in surplusnum) {
+
+      if (productId == i && surplusnum[i] != 0) {
+
+        if (surplusnum[i] >= this.data.buyNum[i]) {
+
+          let count = surplusnum[i] - 1;
+
+          if (count <= 0) {
+
+            surplusnum[i] = 0;
+
+            break;
+
+          } else {
+
+            surplusnum[i] = count;
+
+            break;
+
+          }
+
+        } else {
+
+          break;
+
+        }
+
+      }
+
+    }
+
+    //更新商品剩余
+
+    this.setData({
+
+      surplusNum: surplusnum
+
+    })
+
+  },
+
+
+
+  //减
+
+  subtract: function (e) {
+
+    console.log('点击减号')
+
+    var productId = e.target.dataset.id
+
+    var arrId = [];
+
+    var buyCount = 0;
+
+    //判断商品剩余
+
+    // if (this.data.surplusNum[productId] >= this.data.surplusnumInfo[productId]) {
+
+    //   return false;
+
+    // }
+
+    var arr = this.data.cart || [];
+
+    if (arr.length > 0) {
+
+      for (let i in arr) {
+
+        arrId.push(arr[i].id);
+
+      }
+
+      //商品在购物车中存在
+
+      if (arrId.indexOf(productId) != -1) {
+
+        for (let g in arr) {
+
+          if (arr[g].id == productId) {
+
+            arr[g].buyCount -= 1;
+
+            buyCount = arr[g].buyCount;
+
+            arr[g].buyMoney = this.buyOneMoney(arr[g].price, arr[g].buyCount)
+
+          }
+
+          if (arr[g].buyCount <= 0) {
+
+            this.removeByValue(arr, arr[g].id);
+
+          }
+
+        }
+
+      }
+
+    }
+
+    //判断购物车的商品
+
+    if (arr.length <= 0) {
+
+      this.setData({
+
+        cart: []
+
+      })
+
+    } else {
+
+      this.setData({
+
+        cart: arr
+
+      })
+
+    }
+
+
+
+    var data = { buyNum: this.data.buyNum };
+
+    data.buyNum[productId] = buyCount;
+
+    //用来显示加减购物的数量显示
+
+    this.data.buyNum[productId] = buyCount;
+
+    this.setData(data);
+
+    this.setData({
+
+      sumMoney: this.buySumMoney(arr),
+
+      cart: arr,
+
+      buySum: this.buySum(arr),
+
+    });
+
+    //清空商品id集合
+
+    arrId = [];
+
+    //判断剩余数量(剩余数量)
+
+    var surplusnum = this.data.surplusNum;
+
+    for (var i in surplusnum) {
+
+      if (productId == i && this.data.buyNum[i] >= 0) {
+
+        let count = surplusnum[i] + 1;
+
+        surplusnum[i] = count;
+
+        break;
+
+      }
+
+    }
+
+    //更新商品剩余
+
+    this.setData({
+
+      surplusNum: surplusnum
+
+    })
+
+  },
+
+
+
+  submit: function () {
+
+    //组装下参数
+
+    var params = {};
+
+
+
+  },
+
+
+
+  //购买总数
+
+  buySum: function (array) {
+
+    var sum = 0;
+
+    for (var g in array) {
+
+      if (array[g].buyCount >= 0) {
+
+        sum += array[g].buyCount;
+
+      } else {
+
+        return sum;
+
+      }
+
+    }
+
+    return sum;
+
+  },
+
+
+
+  //单个商品总价
+
+  buyOneMoney: function (price, buyCount) {
+
+    if (buyCount <= 0) {
+
+      return 0.00;
+
+    } else {
+
+      return parseFloat(price * buyCount * 10000000 / 10000000).toFixed(2);
+
+    }
+
+  },
+
+
+
+  //购买总价
+
+  buySumMoney: function (array) {
+
+    var sum = 0.00;
+
+    for (let g in array) {
+
+      if (array[g].buyCount >= 0) {
+
+        sum += (array[g].buyCount * array[g].price * 10000000 / 10000000);
+
+      } else {
+
+        return sum;
+
+      }
+
+    }
+
+    return parseFloat(sum).toFixed(2);
+
+  },
+
+
+
+  //定义根据id删除数组的方法
+
+  removeByValue: function (array, val) {
+
+    for (let i = 0; i < array.length; i++) {
+
+      if (array[i].id == val) {
+
+        array.splice(i, 1);
+
+        break;
+
+      }
+
+    }
+
+  },
+
+
+
+  //左右关系联动
+
+  onGoodsScroll: function (e) {
+
+    if (e.detail.scrollTop > 10 && !this.data.scrollDown) {
+
+      this.setData({
+
+        scrollDown: true
+
+      });
+
+    } else if (e.detail.scrollTop < 10 && this.data.scrollDown) {
+
+      this.setData({
+
+        scrollDown: false
+
+      });
+
+    }
+
+    var scale = e.detail.scrollWidth / 570,
+
+      scrollTop = e.detail.scrollTop / scale,
+
+      h = 0,
+
+      classifySeleted,
+
+      len = this.data.list;
+
+
+
+    var list = this.data.list;
+
+    for (let i in list) {
+
+      //商品的高度也是根据总体的scrow-view来算的
+
+      var goodsHeight = 70 + list[i].goods.length * (46 * 3 + 20 * 2);
+
+      if (scrollTop >= h - 100 / scale) {
+
+        classifySeleted = list[i].id;
+
+      }
+
+      h += goodsHeight;
+
+    }
+
+    this.setData({
+
+      classifySeleted: classifySeleted
+
+    });
+
+  },
+
+
+
+  //左侧点击事件
+
+  tapClassify: function (e) {
+
+    var id = e.target.dataset.id;
+
+    this.setData({
+
+      classifyViewed: id
+
+    });
+
+    var self = this;
+
+    setTimeout(function () {
+
+      self.setData({
+
+        classifySeleted: id
+
+      });
+
+    }, 100);
+
+  },
+
+
+
+  //底部购物车显示
+
+  showCartDetail: function () {
+
+    this.setData({
+
+      showCartDetail: !this.data.showCartDetail
+
+    });
+
+  },
+
+
+
+  hideCartDetail: function () {
+
+    this.setData({
+
+      showCartDetail: false
+
+    });
+
   }
-})
+
+});
