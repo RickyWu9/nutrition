265406c654950df8e279aaa5bfbaee2536e7062b
夷 巫 <1343296078@qq.com>
2020-06-18 13:08:36 +0800

    submit
    上传数据库功能

--- miniprogram/pages/cal/cal.js
+++ miniprogram/pages/cal/cal.js
@@ -450,8 +450,33 @@ Page({
   submit: function (array) {
 
     //组装下参数
-
-    var params = {};
+    console.log(this.data.cart);
+    console.log(this.data.sumMoney);   
+    const db = wx.cloud.database()
+    db.collection('listtest').add({
+      data: {
+        dietList:this.data.cart,
+        calorySum:this.data.sumMoney,
+      },
+      success: res => {
+        // 在返回结果中会包含新创建的记录的 _id
+        this.setData({
+          dietList:this.data.cart,
+          calorySum:this.data.sumMoney,
+        })
+        wx.showToast({
+          title: '新增记录成功',
+        })
+        console.log('[数据库] [新增记录] 成功，记录 _id: ', res._id)
+      },
+      fail: err => {
+        wx.showToast({
+          icon: 'none',
+          title: '新增记录失败'
+        })
+        console.error('[数据库] [新增记录] 失败：', err)
+      }
+    })
 
 
 
